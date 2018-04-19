b8aba834edceda583949165a96bfa7e2f2afb30c
Arseniys1 <thevalakas1@gmail.com>
2018-04-19 15:21:22 +0500

    fix config

--- config/opcache.php
+++ config/opcache.php
@@ -9,7 +9,7 @@ return [
         base_path('resources/lang'),
         base_path('routes'),
         base_path('storage/framework/views'),
-        base_path('vendor/appstract'),
+        base_path('vendor/arseniys1'),
         base_path('vendor/composer'),
         base_path('vendor/laravel/framework'),
     ],
